8000837094dd8f694ac934a4ea275d30083ed7c8
Fathi-Moh <124039464+Fathi-Moh@users.noreply.github.com>
2024-04-02 18:25:25 +0100

    HashID class does not need to be adjusted as the calculation will already be done in the fullNode class

--- src/HashID.java
+++ src/HashID.java
@@ -3,6 +3,7 @@
 //
 // Construct the hashID for a string
 
+import java.lang.StringBuilder;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
@@ -19,38 +20,5 @@ public class HashID {
 			// 2D#4 computes hashIDs of lines, i.e. strings ending with '\n'
 			throw new Exception("No new line at the end of input to HashID");
 		}
-	}
-	// This method calculates the distance between two hashIDs and performs an XOR operation
-	public static int distanceFromHashID(byte[] hashOne, byte[] hashTwo){
-		int distance = 0;
-		for(int i = 0; i < hashOne.length; i++){
-			int XOR = hashOne[i] ^ hashTwo[i];
-			distance += Integer.bitCount(XOR);
-		}
-		return distance;
-	}
-
-	// This method calculates the leading zeros in a hashID represented in a byte array
-	public static int amountOfLeadingZeros(byte[] hash) {
-		int zerosCounter = 0;
-		for (byte b : hash) {
-			if (b != 0) {
-				// Subtract 24 as SHA-256 hash bytes are unsigned
-				zerosCounter += Integer.numberOfLeadingZeros(b) - 24;
-				break;
-			}
-			// 8 bit for a byte
-			zerosCounter += 8;
-		}
-		return zerosCounter;
-	}
-
-	// This method is used to perform a XOR operation on two hashIDs as byte arrays
-	public static byte[] bitwiseXOR(byte[] hashOne, byte[] hashTwo) {
-		byte[] result = new byte[hashOne.length];
-		for (int i = 0; i < hashOne.length; i++) {
-			result[i] = (byte) (hashOne[i] ^ hashTwo[i]);
-		}
-		return result;
 	}
 }
